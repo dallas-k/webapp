cc2e4076ad923c60e3441bd9f3e17e13c8b18ed7
dallas <dallas.wty@gmail.com>
2023-01-03 00:41:05 +0900

    배포 이전 작업 완료

---
+++ frontend/src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Index from '@/components/MovieIndexPage'
+import Show from '@/components/MovieDetailPage'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'index',
+    component: Index
+  },
+  {
+    path: '/:id',
+    name: 'show',
+    component: Show
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
