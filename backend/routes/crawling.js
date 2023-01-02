fd624654a7167a2e1cc5970cf57c9471da89b423
dallas <dallas.wty@gmail.com>
2023-01-03 02:05:47 +0900

    crawling

---
+++ backend/routes/crawling.js
@@ -0,0 +1,44 @@
+      
+const express = require('express');
+const router = express.Router();
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const url = 'https://store.steampowered.com/search/?specials=1&ignore_preferences=1';
+
+const getHtml = async (url) => {
+    try {
+        return await axios.get(url);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    getHtml(url)
+        .then(html => {
+            let ulList = [];
+            const $ = cheerio.load(html.data);
+            const $bodyList = $("div#search_resultsRows").children("a");
+            console.log(typeof($bodyList));
+            $bodyList.each(function(i, elem) {
+                const price = $(this).find('div.search_price').text().replace(/,/gi,"")
+                    .replace(/ /gi,"").split("₩");
+                ulList[i] = {
+                    title: $(this).find('span.title').text(),
+                    sale: $(this).find('div.search_discount span').text(),
+                    original: price[1],
+                    salePrice: price[2],
+                    href: $(this).attr('href'),
+                    imgSrc: $(this).find('div.col.search_capsule img').attr('src'),
+                };
+            });
+            res.send(ulList);
+            // return ulList.filter(n => n.title);
+        })
+        // .then(result => res.render('index', { title: result }));
+    
+});
+
+module.exports = router;
